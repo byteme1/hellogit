c2d9e9257ecd3157356ddb17c5c5ec6ef49990b7
byteme1 <byteme1@users.noreply.github.com>
2019-01-28 20:02:08 -0400

    added numbers

---
+++ vector.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+int main{
+vector<char> v;
+vector<int> v1 = {10, 14, 32, 64, 16};
+
+for (int i = 0; i < v1.size(); i++)
+{
+
+	cout<<v1[i]<<" ";
+}
+
+
+
+
+return 0;
+}
